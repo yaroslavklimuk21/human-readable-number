84ec0a5b30473cb5d21277690a7befb8f6247d5a
yaroslavklimuk21 <yaroslavklimuk21@gmail.com>
2020-10-14 20:23:12 +0300

    init: start human-readable-number task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,59 @@
-module.exports = function toReadable (number) {
-  
-}
+module.exports = function toReadable(number) {
+    var ones = [
+        "",
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+        "ten",
+        "eleven",
+        "twelve",
+        "thirteen",
+        "fourteen",
+        "fifteen",
+        "sixteen",
+        "seventeen",
+        "eighteen",
+        "nineteen",
+    ];
+    var tens = [
+        "",
+        "",
+        "twenty",
+        "thirty",
+        "forty",
+        "fifty",
+        "sixty",
+        "seventy",
+        "eighty",
+        "ninety",
+    ];
+
+    let numString = number.toString();
+
+    if (number === 0) return "zero";
+
+    if (number < 20) {
+        return ones[number];
+    }
+
+    if (numString.length === 2) {
+        return (tens[numString[0]] + " " + ones[numString[1]]).trim();
+    }
+
+    if (numString.length == 3) {
+        if (numString[1] === "0" && numString[2] === "0")
+            return (ones[numString[0]] + " hundred ").trim();
+        else
+            return (
+                ones[numString[0]] +
+                " hundred " +
+                toReadable(+(numString[1] + numString[2]))
+            ).trim();
+    }
+};
